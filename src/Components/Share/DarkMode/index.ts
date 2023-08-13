c858a1c553d02ecdb399775bb5d4cfe5384580aa
Avdushin <avdushinbeaver1@gmail.com>
2023-08-13 04:04:47 +0300

    Added FSD Arch Aliases

--- src/Components/Share/DarkMode/index.ts
+++ src/Components/Share/DarkMode/index.ts
@@ -1,2 +1,4 @@
 import RadioToggler from "./RadioToggler/RadioToggler";
 import CheckBoxToggler from "./CheckBoxToggler/CheckBoxToggler";
+
+// TODO: Обновить файловую структуру в соответствии с FSD архитектурой
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // @Aliases
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, './src'),
+      "@App": path.resolve(__dirname, './src/App'),
+      "@Entities": path.resolve(__dirname, './src/Entities'),
+      "@Pages": path.resolve(__dirname, './src/Pages'),
+      "@Shared": path.resolve(__dirname, './src/Shared'),
+      "@Widgets": path.resolve(__dirname, './src/Widgets'),
+      "@Features": path.resolve(__dirname, './src/Features'),
+      "@Styles": path.resolve(__dirname, './src/App/Styles'),
+      "@Img": path.resolve(__dirname, './src/Shared/assets/img'),
+      "@Icons": path.resolve(__dirname, './src/Shared/assets/icons'),
+    },
+  },
+  // @Build advanced settings
   base: './',
+  // @Plugins
   plugins: [react()],
 })
