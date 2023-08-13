7cf723e3c012afc4247cbd61cc3d8628ed572ba1
Avdushin <avdushinbeaver1@gmail.com>
2023-08-14 02:43:42 +0300

    Added check, if user typing someting at the any input -> sidebar shortcut = off

--- frontend/src/Widgets/UI/Sidebar/index.tsx
+++ frontend/src/Widgets/UI/Sidebar/index.tsx
@@ -1,13 +1,20 @@
 import React, { useEffect } from "react";
 import { SidebarItem } from "./SidebarItem";
-import sidebarLinks from './SideBarLinks';
+import sidebarLinks from "./SideBarLinks";
 import "./sidebar.scss";
 
 const Sidebar = ({ isSidebarVisible, setIsSidebarVisible }) => {
   useEffect(() => {
     const handleKeyDown = (event) => {
-      if (event.key === "m" || event.key === "ь") {
-        setIsSidebarVisible((prevIsSidebarVisible) => !prevIsSidebarVisible);
+      // Проверяем, не находится ли фокус внутри поля ввода
+      if (event.key === "m" || event.key === "ь" || event.keyCode === 75) {
+        const activeElement = document.activeElement;
+        const isInputFocused =
+          activeElement.tagName === "INPUT" && activeElement.type === "text";
+
+        if (!isInputFocused) {
+          setIsSidebarVisible((prevIsSidebarVisible) => !prevIsSidebarVisible);
+        }
       }
     };
 
